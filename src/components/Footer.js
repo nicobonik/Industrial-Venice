9650a441943aad4ddff12bd43c502cfd89c967e2
jhealey <jjheals@gmail.com>
2024-04-28 11:43:25 +0200

    add files

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+// src/components/Footer.js
+
+/** { Component } Footer 
+ * 
+ * @abstract 
+ * 
+ */
+
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/components/Footer.css'; 
+import { LanguageContext } from '../context/LanguageContext.js';
+
+const Footer = () => {
+    const { t, language } = useContext(LanguageContext);
+
+   
+
+    return (
+        <div className='footer'>
+            <Link to='/' className='footer-link'>
+                <div className='footer-button'>{ t('goToHome') }</div>
+            </Link>
+            <Link to='/industrial-sites' className='footer-link'>
+                <div className='footer-button'>{ t('goToIndustrialSites') }</div>
+            </Link>
+            <Link to='industrial-stories' className='footer-link'>
+                <div className='footer-button'>{ t('goToStories') }</div>
+            </Link>
+            <hr className='footer-hr'></hr>
+        </div>
+    );
+};
+
+export default Footer;
